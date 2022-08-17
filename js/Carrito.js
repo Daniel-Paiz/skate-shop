de7522ee99d4ebb4ccb311862deae0d9498f27da
parce <night.paiz@gmail.com>
2022-08-16 23:07:40 -0300

    proceso1.9js

--- js/Carrito.js
+++ js/Carrito.js
@@ -1,37 +1,46 @@
-// Swal.fire({
-//     title: "Submit your Github username",
-//     input: "text",
-//     inputAttributes: {
-//         autocapitalize: "off"
-//     },
-//     showCancelButton: true,
-//     confirmButtonText: "Look up",
-//     showLoaderOnConfirm: true,
-//     preConfirm: (login) => {
-//         return fetch(`//api.github.com/users/${login}`)
-//             .then((response) => {
-//                 if (!response.ok) {
-//                     throw new Error(response.statusText);
-//                 }
-//                 return response.json();
-//             })
-//             .catch((error) => {
-//                 Swal.showValidationMessage(`Request failed: ${error}`);
-//             });
-//     },
-//     allowOutsideClick: () => !Swal.isLoading()
-// }).then((result) => {
-//     if (result.isConfirmed) {
-//         Swal.fire({
-//             title: `${result.value.login}'s avatar`,
-//             imageUrl: result.value.avatar_url
-//         });
-//     }
-// });
-const Swal = require('sweetalert2')
-Swal.fire({
-    title: 'Error!',
-    text: 'Do you want to continue',
-    icon: 'error',
-    confirmButtonText: 'Cool'
+// window.addEventListener('load', () => {
+//     (async () => {
+
+//         const { value: accept } = await Swal.fire({
+//             title: 'Se nesesita ser mayor de edad para proceder con la compra en caso contrario recurir  a padres o tutor a cargo',
+//             input: 'checkbox',
+//             inputValue: 1,
+//             inputPlaceholder:
+//                 'aceptar condiciones',
+//             confirmButtonText:
+//                 'Continue <i class="fa fa-arrow-right"></i>',
+//             inputValidator: (result) => {
+//                 return !result && 'You need to agree with T&C'
+//             }
+//         })
+
+//         if (accept) {
+//             Swal.fire('gracias por confiar en nosotros atte skateshop campana')
+//         }
+//     })()
+// })
+
+function obtenerHTMLPorProductodecarrito(producto) {
+    return `
+        <tr>
+            <td>${producto.nombre}</td>
+            <td>${producto.price}</td>
+            <td>
+                <button id="${producto.type}_${producto.id}" class="btn btn-primary" type="button">eliminar</button>
+            </td>
+        </tr>
+    `;
+}
+function prepararBotonesBorrarDeCarrito(producto) {
+    const botonBorrarProducto = document.getElementById(`${producto.type}_${producto.id}`);
+    botonBorrarProducto.addEventListener("click", () => {
+        borrarProductoDelcarrito(producto);
+    });
+}
+window.addEventListener("load", () => {
+    const contenedorDeProductos = document.getElementById("tablaDeCarrito");
+    tablas.forEach((tabla) => {
+        contenedorDeProductos.innerHTML += obtenerHTMLPorProductodecarrito(tabla);
+    });
+    tablas.forEach(prepararBotonesBorrarDeCarrito);
 })
